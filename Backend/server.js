fdf893d5cd818651b4adff27edadfafe2d460983
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-15 20:37:44 +0530

    add backend frontend connectivity

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,23 @@
 import express from 'express'
+import cors from 'cors'
 
 const app= express();
 const PORT= process.env.PORT;
-app.use()
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.post('/login',(req,res)=>{
     const {username,password}= req.body;
+    console.log(username,password);
+    res.send("data");
+})
 
+app.post('/register',(req,res)=>{
+    const {username,password}= req.body;
+    console.log(username,password);
+    res.send("data");
 })
 
 app.listen(PORT,()=>{
